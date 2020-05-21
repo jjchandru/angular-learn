946fe06981c04d435c28cd98cf683550c9a1399c
Chandrasekaran Janardhanan <jjchandru@yahoo.com>
2020-05-21 15:02:23 +0530

    genres implemented with reactive form

--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 import { Genre } from '../movie/model/genre';
 import { genres } from '../data';
 
@@ -9,15 +10,41 @@ import { genres } from '../data';
   styleUrls: ['./genres.component.css']
 })
 export class GenresComponent implements OnInit {
-  genreForm: FormGroup = this.fb.group({
-    id: [''],
-    name: ['']
+  genresForm: FormGroup = this.fb.group({
+    genres: this.fb.array([])
   });
+  itemCount = 0;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.genreForm.patchValue(genres[2]);
+    let items = this.genresForm.get('genres') as FormArray;
+    this.itemCount = genres.length;
+    for (let i = 0; i < genres.length; i++) {
+      items.push(this.fb.group({
+        id: genres[i].id,
+        name: genres[i].name
+      }));
+    }
+    console.log(JSON.stringify(this.genresForm.value));
+  }
+
+  get genres() {
+    return this.genresForm.get('genres') as FormArray;
+  }
+
+  addGenre() {
+    let items = this.genresForm.get('genres') as FormArray;
+    let id = this.itemCount + 1;
+    items.push(this.fb.group({
+      id: id,
+      name: ''
+    }));
+  }
+
+  deleteGenre(index: number) {
+    let items = this.genresForm.get('genres') as FormArray;
+    items.removeAt(index);
   }
 
 }
